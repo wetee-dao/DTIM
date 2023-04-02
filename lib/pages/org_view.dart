6b09a427e0a3213abd2855abacb0db6c5d84e764
baiL <422145328@qq.com>
2023-04-02 09:17:28 +0800

    更新 org view

--- lib/pages/org_view.dart
+++ lib/pages/org_view.dart
@@ -112,217 +112,221 @@ class _OrgViewPageState extends State<OrgViewPage> {
   @override
   Widget build(BuildContext context) {
     final constTheme = Theme.of(context).extension<ExtColors>()!;
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          moveWindow(
-            Row(
-              children: [
-                SizedBox(width: 15.w),
-                BasePopupMenu(
-                  verticalMargin: -1.w,
-                  horizontalMargin: 5.w,
-                  showArrow: false,
-                  controller: menuController,
-                  pressType: PressType.singleClick,
-                  position: PreferredPosition.bottomLeft,
-                  child: StreamBuilder<bool>(
-                    stream: menuStreamController.stream,
-                    initialData: false,
-                    builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
-                      return Container(
-                        height: 40.w,
-                        padding: EdgeInsets.only(left: 10.w, right: 7.w, top: 2.w, bottom: 2.w),
-                        decoration: BoxDecoration(
-                          color: snapshot.data != null && snapshot.data!
-                              ? constTheme.sidebarText.withOpacity(0.25)
-                              : constTheme.sidebarText.withOpacity(0.1),
-                          borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(3.w),
-                            bottomLeft: Radius.circular(3.w),
+    return Column(
+      children: [
+        moveWindow(
+          Row(
+            children: [
+              SizedBox(width: 15.w),
+              BasePopupMenu(
+                verticalMargin: -1.w,
+                horizontalMargin: 5.w,
+                showArrow: false,
+                controller: menuController,
+                pressType: PressType.singleClick,
+                position: PreferredPosition.bottomLeft,
+                child: StreamBuilder<bool>(
+                  stream: menuStreamController.stream,
+                  initialData: false,
+                  builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
+                    return Container(
+                      height: 40.w,
+                      padding: EdgeInsets.only(left: 10.w, right: 7.w, top: 2.w, bottom: 2.w),
+                      decoration: BoxDecoration(
+                        color: snapshot.data != null && snapshot.data!
+                            ? constTheme.sidebarText.withOpacity(0.25)
+                            : constTheme.sidebarText.withOpacity(0.1),
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(3.w),
+                          bottomLeft: Radius.circular(3.w),
+                        ),
+                      ),
+                      child: Row(
+                        children: [
+                          Text(
+                            org != null ? org!.orgName ?? "" : '',
+                            style: TextStyle(
+                              color: constTheme.sidebarText,
+                              fontWeight: FontWeight.w800,
+                              fontSize: 13.w,
+                              height: 1.3,
+                            ),
+                          ),
+                          Icon(
+                            Icons.keyboard_arrow_down_outlined,
+                            color: constTheme.sidebarText,
+                            size: 16.w,
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+                menuBuilder: () => orgMenuRender(menuController),
+              ),
+              SizedBox(width: 1.w),
+              Expanded(
+                child: JumpTo(false, MediaQuery.of(context).size.width - 40, () {
+                  showModelOrPage(context, "/search");
+                  return null;
+                }),
+              )
+            ],
+          ),
+        ),
+        Divider(
+          height: 1,
+          color: constTheme.sidebarText.withOpacity(0.08),
+        ),
+        SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.only(left: 15.w, right: 15.w, top: 10.w, bottom: 5.w),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Expanded(
+                      child: Text(
+                        L10n.of(context)!.channel,
+                        style: TextStyle(
+                          color: constTheme.sidebarText.withAlpha(180),
+                          fontWeight: FontWeight.w800,
+                          fontSize: 14.w,
+                        ),
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        GestureDetector(
+                          key: const Key("create_channel"),
+                          onTap: () async {
+                            showModelOrPage(
+                              context,
+                              "/create_channel",
+                              width: 450.w,
+                              height: 300.w,
+                            );
+                          },
+                          child: Icon(
+                            Icons.add,
+                            size: 20.w,
+                            color: constTheme.sidebarText.withAlpha(180),
                           ),
                         ),
-                        child: Row(
-                          children: [
-                            Text(
-                              org != null ? org!.orgName ?? "" : '',
-                              style: TextStyle(
-                                color: constTheme.sidebarText,
-                                fontWeight: FontWeight.w800,
-                                fontSize: 13.w,
-                                height: 1.3,
-                              ),
-                            ),
-                            Icon(
-                              Icons.keyboard_arrow_down_outlined,
-                              color: constTheme.sidebarText,
-                              size: 16.w,
-                            ),
-                          ],
+                        SizedBox(width: 5.w),
+                        GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              _controllerChannels.toggle();
+                            });
+                          },
+                          child: Icon(
+                            _controllerChannels.expanded
+                                ? Icons.keyboard_arrow_down_outlined
+                                : Icons.keyboard_arrow_up_outlined,
+                            size: 25.w,
+                            color: constTheme.sidebarText.withAlpha(180),
+                          ),
                         ),
-                      );
-                    },
-                  ),
-                  menuBuilder: () => orgMenuRender(menuController),
-                ),
-                SizedBox(width: 1.w),
-                Expanded(
-                  child: JumpTo(false, MediaQuery.of(context).size.width - 40, () {
-                    showModelOrPage(context, "/search");
-                    return null;
-                  }),
-                )
-              ],
-            ),
-          ),
-          Divider(
-            height: 1,
-            color: constTheme.sidebarText.withOpacity(0.08),
-          ),
-          Padding(
-            padding: EdgeInsets.only(left: 15.w, right: 15.w, top: 10.w, bottom: 5.w),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Expanded(
-                  child: Text(
-                    L10n.of(context)!.channel,
-                    style: TextStyle(
-                      color: constTheme.sidebarText.withAlpha(180),
-                      fontWeight: FontWeight.w800,
-                      fontSize: 14.w,
-                    ),
-                  ),
-                ),
-                Row(
-                  children: [
-                    GestureDetector(
-                      key: const Key("create_channel"),
-                      onTap: () async {
-                        showModelOrPage(
-                          context,
-                          "/create_channel",
-                          width: 450.w,
-                          height: 300.w,
-                        );
-                      },
-                      child: Icon(
-                        Icons.add,
-                        size: 20.w,
-                        color: constTheme.sidebarText.withAlpha(180),
-                      ),
-                    ),
-                    SizedBox(width: 5.w),
-                    GestureDetector(
-                      onTap: () {
-                        setState(() {
-                          _controllerChannels.toggle();
-                        });
-                      },
-                      child: Icon(
-                        _controllerChannels.expanded
-                            ? Icons.keyboard_arrow_down_outlined
-                            : Icons.keyboard_arrow_up_outlined,
-                        size: 25.w,
-                        color: constTheme.sidebarText.withAlpha(180),
-                      ),
-                    ),
+                      ],
+                    )
                   ],
-                )
-              ],
-            ),
-          ),
-          ExpandablePanel(
-            key: const Key("room"),
-            controller: _controllerChannels,
-            collapsed: const SizedBox(),
-            expanded: ChatList(
-              key: const Key("ChatList"),
-              channels,
-              channelId,
-              (id) {
-                setChannelId(id);
-              },
-            ),
-          ),
-          SizedBox(height: 5.w),
-          Divider(
-            height: 1,
-            color: constTheme.sidebarText.withOpacity(0.05),
-          ),
-          Padding(
-            padding: EdgeInsets.only(left: 15.w, right: 15.w, top: 10.w, bottom: 5.w),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Expanded(
-                  child: Text(
-                    L10n.of(context)!.directChat,
-                    style: TextStyle(
-                      fontWeight: FontWeight.w800,
-                      fontSize: 14.w,
-                      color: constTheme.sidebarText.withAlpha(180),
-                    ),
-                  ),
                 ),
-                Row(
+              ),
+              ExpandablePanel(
+                key: const Key("room"),
+                controller: _controllerChannels,
+                collapsed: const SizedBox(),
+                expanded: ChatList(
+                  key: const Key("ChatList"),
+                  channels,
+                  channelId,
+                  (id) {
+                    setChannelId(id);
+                  },
+                ),
+              ),
+              SizedBox(height: 5.w),
+              Divider(
+                height: 1,
+                color: constTheme.sidebarText.withOpacity(0.05),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 15.w, right: 15.w, top: 10.w, bottom: 5.w),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    GestureDetector(
-                      key: const Key("create_private"),
-                      onTap: () async {
-                        showModelOrPage(
-                          context,
-                          "/create_private",
-                          width: 550.w,
-                          height: 0.7.sh,
-                        );
-                      },
-                      child: Icon(
-                        Icons.add,
-                        size: 20.w,
-                        color: constTheme.sidebarText.withAlpha(180),
+                    Expanded(
+                      child: Text(
+                        L10n.of(context)!.directChat,
+                        style: TextStyle(
+                          fontWeight: FontWeight.w800,
+                          fontSize: 14.w,
+                          color: constTheme.sidebarText.withAlpha(180),
+                        ),
                       ),
                     ),
-                    SizedBox(width: 5.w),
-                    GestureDetector(
-                      onTap: () {
-                        setState(() {
-                          _controllerUsers.toggle();
-                        });
-                      },
-                      child: Icon(
-                        _controllerUsers.expanded
-                            ? Icons.keyboard_arrow_down_outlined
-                            : Icons.keyboard_arrow_up_outlined,
-                        size: 25.w,
-                        color: constTheme.sidebarText.withAlpha(180),
-                      ),
+                    Row(
+                      children: [
+                        GestureDetector(
+                          key: const Key("create_private"),
+                          onTap: () async {
+                            showModelOrPage(
+                              context,
+                              "/create_private",
+                              width: 550.w,
+                              height: 0.7.sh,
+                            );
+                          },
+                          child: Icon(
+                            Icons.add,
+                            size: 20.w,
+                            color: constTheme.sidebarText.withAlpha(180),
+                          ),
+                        ),
+                        SizedBox(width: 5.w),
+                        GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              _controllerUsers.toggle();
+                            });
+                          },
+                          child: Icon(
+                            _controllerUsers.expanded
+                                ? Icons.keyboard_arrow_down_outlined
+                                : Icons.keyboard_arrow_up_outlined,
+                            size: 25.w,
+                            color: constTheme.sidebarText.withAlpha(180),
+                          ),
+                        ),
+                      ],
                     ),
                   ],
                 ),
-              ],
-            ),
+              ),
+              ExpandablePanel(
+                key: const Key("droom"),
+                controller: _controllerUsers,
+                collapsed: const SizedBox(),
+                expanded: DirectChats(
+                  key: const Key("DirectChats"),
+                  directChats,
+                  channelId,
+                  (id) {
+                    setChannelId(id);
+                  },
+                ),
+              ),
+              SizedBox(height: 5.w),
+              Divider(
+                height: 1,
+                color: constTheme.sidebarText.withOpacity(0.05),
+              ),
+            ],
           ),
-          ExpandablePanel(
-            key: const Key("droom"),
-            controller: _controllerUsers,
-            collapsed: const SizedBox(),
-            expanded: DirectChats(
-              key: const Key("DirectChats"),
-              directChats,
-              channelId,
-              (id) {
-                setChannelId(id);
-              },
-            ),
-          ),
-          SizedBox(height: 5.w),
-          Divider(
-            height: 1,
-            color: constTheme.sidebarText.withOpacity(0.05),
-          ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
