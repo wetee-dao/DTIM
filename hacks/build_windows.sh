961d7f591c803b830f1c6abff7ea64604871c07f
baiL <bai@asyou.me>
2023-05-03 07:22:55 +0800

    暂时解决linux rust依赖编译问题

---
+++ hacks/build_windows.sh
@@ -0,0 +1,16 @@
+# 获取当前shell文件的路径
+SOURCE="$0"
+while [ -h "$SOURCE"  ]; do
+    DIR="$( cd -P "$( dirname "$SOURCE"  )" && pwd  )"
+    SOURCE="$(readlink "$SOURCE")"
+    [[ $SOURCE != /*  ]] && SOURCE="$DIR/$SOURCE"
+done
+DIR="$( cd -P "$( dirname "$SOURCE"  )" && pwd  )"
+CRATE="$DIR/../rust_wraper"
+
+echo "RUST项目根目录 => $CRATE"
+
+cd "$DIR/../rust_wraper"
+
+cargo build --release
+cp -r "$DIR/../rust_wraper/target/debug/rust_wraper.dll" "$DIR/../windows/shared/rust_wraper.dll"
