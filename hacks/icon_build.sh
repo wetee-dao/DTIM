8a0b85371ac4ea361651899cd1dc04110033a6dc
baiL <422145328@qq.com>
2023-05-06 21:09:29 +0800

    添加文件编译文件

---
+++ hacks/icon_build.sh
@@ -0,0 +1,12 @@
+# 获取当前shell文件的路径
+SOURCE="$0"
+while [ -h "$SOURCE"  ]; do
+    DIR="$( cd -P "$( dirname "$SOURCE"  )" && pwd  )"
+    SOURCE="$(readlink "$SOURCE")"
+    [[ $SOURCE != /*  ]] && SOURCE="$DIR/$SOURCE"
+done
+DIR="$( cd -P "$( dirname "$SOURCE"  )" && pwd  )"
+cd "$DIR/../"
+
+rm ./lib/components/appicon.dart
+iconfont_builder --from ./assets/fonts --to ./lib/components/appicon.dart  --class Appicon
