ea1b5bff9afeea8e9e2008ea5ef497dece8723f2
胡文涛 <35894003+Hu-Wentao@users.noreply.github.com>
2023-06-12 21:23:11 +0800

    move preloader.dart

--- lib/infra/router/router.dart
+++ lib/infra/router/router.dart
@@ -1,4 +1,4 @@
-import 'package:asyou_app/preloader.dart';
+import 'package:asyou_app/infra/pages/preloader.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 
